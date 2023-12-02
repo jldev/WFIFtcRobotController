612fd479a7e190925375a3282dc78fd1f5bde335
sawye <sawyerjsmith99@gmail.com>
2023-12-01 19:04:58 -0600

    added a Neptune Teleop and got the code ready for our new robot B)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/Neptune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/Neptune.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Neptune;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
@@ -7,5 +8,9 @@ import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
 public class Neptune extends SampleMecanumDrive {
     public Neptune(HardwareMap hardwareMap) {
         super(hardwareMap);
+    }
+
+    public void init (HardwareMap hwMap){
+
     }
 }
