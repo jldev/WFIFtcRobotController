d61584507ad233fbca047408024220e4ed420528
jlunn-cslabs <jlunn@cslabs.tech>
2023-12-25 16:33:17 -0600

    Added config annotation and remove final from constants so that they should show up in FTC Dashboard to assist with us tuning them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/NeptuneConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/NeptuneConstants.java
@@ -1,16 +1,19 @@
 package org.firstinspires.ftc.teamcode.Neptune;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class NeptuneConstants {
-    public static final double NEPTUNE_INTAKE_MOTOR_INTAKE_POWER = 0.5;
-    public static final double NEPTUNE_INTAKE_MOTOR_EJECT_POWER = -0.5;
-    public static final double NEPTUNE_INTAKE_SERVO_POS1 = 0.0;
-    public static final double NEPTUNE_INTAKE_SERVO_POS2 = 1.0;
-    public static final int NEPTUNE_SLIDE_POS1 = 1400;
-    public static final int NEPTUNE_SLIDE_POS2 = 2800;
-    public static final double NEPTUNE_SLIDE_VBAR_POS_COEFFICIENT = 0.1;
-    public static final int NEPTUNE_SLIDE_VBAR_POS_TOLERANCE = 20;
-    public static final double NEPTUNE_SLIDE_MOTOR_POS_COEFFICIENT = 0.1;
-    public static final int NEPTUNE_SLIDE_MOTOR_POS_TOLERANCE = 20;
-    public static final int NEPTUNE_VBAR_MOTOR_TARGET_POSITION_UP = 500;
-    public static final int NEPTUNE_VBAR_MOTOR_TARGET_POSITION_DOWN = 0;
+    public static double NEPTUNE_INTAKE_MOTOR_INTAKE_POWER = 0.5;
+    public static double NEPTUNE_INTAKE_MOTOR_EJECT_POWER = -0.5;
+    public static double NEPTUNE_INTAKE_SERVO_POS1 = 0.0;
+    public static double NEPTUNE_INTAKE_SERVO_POS2 = 1.0;
+    public static int NEPTUNE_SLIDE_POS1 = 1400;
+    public static int NEPTUNE_SLIDE_POS2 = 2800;
+    public static double NEPTUNE_SLIDE_VBAR_POS_COEFFICIENT = 0.1;
+    public static int NEPTUNE_SLIDE_VBAR_POS_TOLERANCE = 20;
+    public static double NEPTUNE_SLIDE_MOTOR_POS_COEFFICIENT = 0.1;
+    public static int NEPTUNE_SLIDE_MOTOR_POS_TOLERANCE = 20;
+    public static int NEPTUNE_VBAR_MOTOR_TARGET_POSITION_UP = 500;
+    public static int NEPTUNE_VBAR_MOTOR_TARGET_POSITION_DOWN = 0;
 }
