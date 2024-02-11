5971e51c641e5d4b9a79bcfdc1bace0b2eaff97b
sawye <sawyerjsmith99@gmail.com>
2024-02-10 22:17:28 -0600

    code changes made at meridian qualifer :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/NeptuneConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neptune/NeptuneConstants.java
@@ -26,7 +26,7 @@ public class NeptuneConstants {
     public static double NEPTUNE_HANG_REST_POS= 1.0;
     public static double NEPTUNE_HANG_POS= 0.65;
 
-    public static double NEPTUNE_DISTANCESENSOR_POS = 6;
+    public static double NEPTUNE_DISTANCESENSOR_POS = 4;
 
     public static double NEPTUNE_SECONDHANG_POS = 0.8;
     public static double NEPTUNE_HANG_MOTOR_UP_POWER = 1;
@@ -38,7 +38,7 @@ public class NeptuneConstants {
     public static double OUTAKE_OPEN_POSITION = 0.5;
     public static double OUTAKE_CLOSED_POSITION = 0.0;
     public static double AUTOOUTAKE_OPEN_POSITION = 0.4;
-    public static double AUTOOUTAKE_CLOSED_POSITION = 0.75;
+    public static double AUTOOUTAKE_CLOSED_POSITION = 0.6;
 
     public static double DRONE_LAUNCHED = 0.5;
 
